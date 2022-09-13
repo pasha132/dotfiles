f6d34f85ba9879e2e91a15d719516f062963ceda
Pavel Andreev <pasha132@gmail.com>
2022-09-13 20:35:52 +0300

    wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,12 @@
+
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font {
+    family = 'Iosevka Term',
+    stretch = 'Expanded',
+    weight = 'Regular',
+  },
+  font_size = 16,
+  color_scheme = 'Breeze',
+  enable_tab_bar = false,
+}
