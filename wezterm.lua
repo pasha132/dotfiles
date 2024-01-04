7bec6dd7a1ea6b2ab62ad795b9b3c767a6ac2644
Pavel Andreev <pasha132@gmail.com>
2024-01-04 12:46:46 +0300

    update configs

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,9 @@
-
 local wezterm = require 'wezterm'
+local act = wezterm.action
+
 return {
+  check_for_updates = false,
+
   font = wezterm.font {
     family = 'Iosevka Term',
     stretch = 'Expanded',
@@ -9,4 +12,25 @@ return {
   font_size = 16,
   color_scheme = 'Breeze',
   enable_tab_bar = false,
+  window_padding = {
+    left = '1cell',
+    right = '1cell',
+    top = 0,
+    bottom = 0,
+  },
+  mouse_bindings = {
+    -- Change the default click behavior so that it only selects
+    -- text and doesn't open hyperlinks
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'NONE',
+      action = act.CompleteSelection 'PrimarySelection',
+    },
+    -- and make CTRL-Click open hyperlinks
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'CTRL',
+      action = act.OpenLinkAtMouseCursor,
+    },
+  },
 }
